7c69d7c82cf472d5f9f7e947cff2f2bd41d869c7
Lucas Santos <lucas@lucass.io>
2025-11-06 19:45:27 -0300

    feat(tokens-repositories): ADD refresh and access tokens DB logic

---
+++ src/repositories/access-token.ts
@@ -0,0 +1,48 @@
+import cache_db from "../db/redis/redis";
+
+const saveAccessToken = async (tokenUUID: string, userUUID: string, ) => {
+
+    `
+        Save a access token in Redis with the following structure:
+        Key: access_token:{tokenUUID}
+        Value: Hash with field 'user_uuid' set to userUUID
+    `
+
+    return await cache_db.hSet('Access_token:' + tokenUUID, {
+        user_uuid: userUUID
+    });
+
+}
+
+const getAccessToken = async (token_UUID: string) => {
+
+
+    `
+            Get the user_uuid associated with the given Access token UUID.
+    `
+
+    return await cache_db.hGet('Access_token:' + token_UUID, 'user_uuid');
+
+}
+
+const delAccessToken = async (token_UUID: string) => {
+
+    `
+        Delete a Access token from Redis by its UUID.
+    `
+
+    return await cache_db.del('Access_token:' + token_UUID);
+
+}
+
+const consumeAccessToken = async (token_UUID: string) => {
+
+    `
+        Return and delete the user_uuid associated with the given Access token UUID.
+    `
+
+    return await cache_db.hGetDel('Access_token:' + token_UUID, 'user_uuid');
+
+}
+
+export {saveAccessToken, delAccessToken, consumeAccessToken, getAccessToken};
--- src/repositories/refresh-token.ts
+++ src/repositories/refresh-token.ts
@@ -1,13 +1,43 @@
-const saveInRedisRefreshToken = async (UUID: string) => {
+import cache_db from "../db/redis/redis";
 
-    // Inforcações necessárias?
+const saveRefreshToken = async (token_UUID: string, user_UUID: string) => {
+
+    `
+        Save a refresh token in Redis with the following structure:
+        Key: refresh_token:{tokenUUID}
+        Value: Hash with field 'user_uuid' set to userUUID
+    `
+
+    return await cache_db.hSet('refresh_token:' + token_UUID, {
+        user_uuid: user_UUID
+    });
 
 }
 
-const delFromRedisRefreshToken = async (tokenUUID: string) => {
+const getRefreshToken = async (token_UUID: string) => {
 
-    // Inforcações necessárias?
+    return await cache_db.hGet('Access_token:' + token_UUID, 'user_uuid');
 
 }
 
-export {saveInRedisRefreshToken, delFromRedisRefreshToken};
+const delRefreshToken = async (token_UUID: string) => {
+
+    `
+        Delete a refresh token from Redis by its UUID.
+    `
+
+    return await cache_db.del('refresh_token:' + token_UUID);
+
+}
+
+const consumeRefreshToken = async (token_UUID: string) => {
+
+    `
+        Return and delete the user_uuid associated with the given refresh token UUID.
+    `
+
+    return await cache_db.hGetDel('refresh_token:' + token_UUID, 'user_uuid');
+
+}
+
+export {saveRefreshToken, delRefreshToken, consumeRefreshToken, getRefreshToken};
