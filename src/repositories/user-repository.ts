6699edeb431f6dcd3779aead0b88bc8c41de9e46
Lucas Santos <lucas@lucass.io>
2025-11-12 10:41:41 -0300

    feat(user-repository): ADD user DB insertes, updates and selects to users table

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -1,3 +1,45 @@
-const creatUserRepository = () => {
-    // Implementation here
+import { db } from "../db/drizzle/drizzle"
+import { eq } from "drizzle-orm"
+import { users } from "../db/drizzle/dbschema"
+
+type UserCredentials = {
+    uuid: string
+    name: string;
+    email: string;
+    passwordHash: string;
 }
+
+const saveUserCredentials = async (credentials: UserCredentials) => {
+    return await db.insert(users).values({
+        uuid: credentials.uuid,
+        username: credentials.name,
+        email: credentials.email,
+        password_hash: credentials.passwordHash,
+    });
+}
+
+const getUserByUUID = async (uuid: string) => {
+    return await db.select().from(users).where(eq(users.uuid, uuid)).limit(1);
+}
+
+const getUserByEmail = async (email: string) => {
+    return await db.select().from(users).where(eq(users.email, email)).limit(1);
+}
+
+const changeUserName = async (uuid: string, newName: string) => {
+    return await db.update(users).set({ username: newName }).where(eq(users.uuid, uuid));
+}
+
+const changeUserEmail = async (uuid: string, newEmail: string) => {
+    return await db.update(users).set({ email: newEmail }).where(eq(users.uuid, uuid));
+}
+
+const changePasswordHash = async (uuid: string, newPasswordHash: string) => {
+    return await db.update(users).set({ password_hash: newPasswordHash }).where(eq(users.uuid, uuid));
+}
+
+const desativateUser = async (uuid: string) => {
+    return await db.update(users).set({ desactivated: true }).where(eq(users.uuid, uuid));
+} 
+
+export {saveUserCredentials, getUserByUUID, getUserByEmail, changeUserName, changeUserEmail, changePasswordHash, desativateUser};
--- tests/integration/repositories/user-repository.spec.ts
+++ tests/integration/repositories/user-repository.spec.ts
@@ -1,11 +1,116 @@
-import {db} from '../../../src/db/drizzle/drizzle'
+import {db, pool} from '../../../src/db/drizzle/drizzle'
+import {users} from '../../../src/db/drizzle/dbschema'
+import {eq} from 'drizzle-orm'
+import {saveUserCredentials, getUserByUUID, changeUserName, changeUserEmail, changePasswordHash, desativateUser} from '../../../src/repositories/user-repository'
 
-describe('', () => {
+describe('Test user repository with DataBase', () => {
 
-    
+    const mockUser = {
+        uuid: '019a782b-839a-76c2-8d83-5f49860f8248',
+        name: 'Test User Two',
+        email: 'test2@mails.com',
+        passwordHash: '527a695e46487d572f6eb2cd50ce2b0c'
+    };
+
+    beforeAll(async () => {
+        await db.delete(users).where(eq(users.email, mockUser.email)).execute();
+    });
+
+    afterAll(async () => {
+        await pool.end();
+    });
 
     it('should insert a user', async () => {
-        // Test implementation here
+
+        await saveUserCredentials({
+            uuid: mockUser.uuid,
+            name: mockUser.name,
+            email: mockUser.email,
+            passwordHash: mockUser.passwordHash
+        });
+
+        const [user] = await db.select().from(users).where(eq(users.uuid, mockUser.uuid)).limit(1)
+
+        expect(user).toBeDefined();
+        expect(user.username).toBe(mockUser.name);
+        expect(user.email).toBe(mockUser.email);
+        expect(user.password_hash).toBe(mockUser.passwordHash);
+
+    });
+
+    it('should return the correctly user by UUID', async () => {
+
+        const [user] = await getUserByUUID(mockUser.uuid);
+
+        expect(user).toBeDefined();
+        expect(user.username).toBe(mockUser.name);
+        expect(user.email).toBe(mockUser.email);
+        expect(user.password_hash).toBe(mockUser.passwordHash);
+
+    });
+
+    it('should return null for non-existing UUID', async () => {
+        const [user] = await getUserByUUID('019a7845-c18b-71a2-85da-cc9dd9a41b80');
+
+        expect(user).toBeUndefined();
+    });
+
+    it('should return a user by email', async () => {
+        const [user] = await db.select().from(users).where(eq(users.email, mockUser.email)).limit(1);
+
+        expect(user).toBeDefined();
+        expect(user.username).toBe(mockUser.name);
+        expect(user.email).toBe(mockUser.email);
+        expect(user.password_hash).toBe(mockUser.passwordHash);
+    });
+
+    it('should return null for non-existing email', async () => {
+        const [user] = await db.select().from(users).where(eq(users.email, 'testeNull@mail.com')).limit(1);
+
+        expect(user).toBeUndefined();
+    });
+
+    it('should update a user name', async () => {
+        const newName = 'Updated Test User Two';
+
+        await changeUserName(mockUser.uuid, newName);
+
+        const [updatedUser] = await db.select().from(users).where(eq(users.uuid, mockUser.uuid)).limit(1);
+
+        expect(updatedUser).toBeDefined();
+        expect(updatedUser.username).toBe(newName);
+    });
+
+    it('should update a user email', async () => {
+        const newEmail= 'newTestMail@mail.com'
+
+        await changeUserEmail(mockUser.uuid, newEmail);
+
+        const [updatedUser] = await db.select().from(users).where(eq(users.uuid, mockUser.uuid)).limit(1);
+
+        expect(updatedUser).toBeDefined();
+        expect(updatedUser.email).toBe(newEmail);
+    });
+
+    it('should update a user password hash', async () => {
+        const newPasswordHash = '827a695e46487d572f6eb2cd50ce2b0c'
+
+        await changePasswordHash(mockUser.uuid, newPasswordHash);
+
+        const [updatedUser] = await db.select().from(users).where(eq(users.uuid, mockUser.uuid)).limit(1);
+
+        expect(updatedUser).toBeDefined();
+        expect(updatedUser.password_hash).toBe(newPasswordHash);
+
+    }); 
+
+    it('should desativate a user', async () => {
+        await desativateUser(mockUser.uuid);
+
+        const [updatedUser] = await db.select().from(users).where(eq(users.uuid, mockUser.uuid)).limit(1);
+
+        expect(updatedUser).toBeDefined();
+        expect(updatedUser.desactivated).toBe(true);
     });
 
 });
