da403df58f8f5d7b3b1c690aed36d9aea97a3908
Lucas Santos <lucas@lucass.io>
2025-11-17 15:56:09 -0300

    refactor: add register path with all register combined

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,11 @@
 import "dotenv/config";
 import fastify from "fastify";
-import routes from "./routes/index";
-import fastifyCookie, { type FastifyCookieOptions } from "@fastify/cookie";
-import rateLimit from "@fastify/rate-limit";
+import fastifyRegisters from "./registers/index";
 
 const app = fastify({ logger: false });
 
-await app.register(rateLimit, {
-    global: false, // If true, applies to all routes
-});
-
-await app.register(fastifyCookie, {
-    secret: process.env.APPLICATION_SECRET || 'default'
-} as FastifyCookieOptions)
-
-app.setErrorHandler((error, request, reply) => {
-
-    if (error.statusCode === 429) {
-        reply.status(429).send(error);
-        return;
-    }
-
-    app.log.error(`Error occurred: ${error.message}`);
-    reply.status(500).send({ error: 'Internal Server Error' });
-});
-
-await routes(app);
-
-app.setNotFoundHandler({ //
-    preHandler: app.rateLimit({
-        max: 5,
-        timeWindow: '1 minute'
-    })
-}, function (request, reply) {
-    reply.code(404).send({ error: 'Not found' })
-})
+// All registers
+await fastifyRegisters(app);
 
 const PORT: number = Number(process.env.PORT) || 3000;
 
