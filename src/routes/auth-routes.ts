576196064df48a4da0b73c2cf8dfa8d526a96d7a
Lucas Santos <lucas@lucass.io>
2025-11-17 15:39:01 -0300

    feat: add rate limit config to register and login route

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -1,4 +1,5 @@
 import type { FastifyInstance } from "fastify";
+import rateLimitConfig from "../config/rate-limit-config";
 
 // pre-validation import
 import { loginCredentialsCheck, registerCredentialsCheck } from "../middlewares/auth";
@@ -15,13 +16,16 @@ const authRoutes = async (app: FastifyInstance): Promise<void> => {
 
     // Login route - Return refresh token + access token
     app.post('/login', {
+        config: { rateLimit: rateLimitConfig },
         schema: loginJsonSchema,
         preValidation: [loginCredentialsCheck],
         handler: userLoginController
-    });
+        }
+    );
 
     // User registration route - Create new user
     app.post('/register', {
+        config: { rateLimit: rateLimitConfig },
         schema: registerJsonSchema,
         preValidation: [registerCredentialsCheck],
         handler: userRegisterController
